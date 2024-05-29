d47fb08ced7475a1a1bdc42937c54c5bde322e17
unknown <swangai7178@gmail.com>
2024-05-29 16:35:21 +0300

    finishing up on the homepage look

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Weather App',
-      home: GetStarted(),
+      home: GetStartedScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/screens/getstarted.dart
+++ lib/screens/getstarted.dart
@@ -1,17 +1,49 @@
+// ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
-
 import 'package:weather_app/models/constant.dart';
-import 'package:weather_app/screens/home.dart';
+import 'home.dart'; 
 
+class GetStartedScreen extends StatefulWidget {
+  const GetStartedScreen({super.key});
 
-class GetStarted extends StatelessWidget {
-  const GetStarted({super.key});
+  @override
+  _GetStartedScreenState createState() => _GetStartedScreenState();
+}
+
+class _GetStartedScreenState extends State<GetStartedScreen> with SingleTickerProviderStateMixin {
+  AnimationController? _animationController;
+  Animation<double>? _fadeInAnimation;
+  Animation<Offset>? _slideAnimation;
+  Constants myConstants = Constants();
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _fadeInAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(parent: _animationController!, curve: Curves.easeIn),
+    );
+
+    _slideAnimation = Tween<Offset>(begin: const Offset(0, 1), end: Offset.zero).animate(
+      CurvedAnimation(parent: _animationController!, curve: Curves.easeInOut),
+    );
+
+    _animationController!.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController!.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    Constants myConstants = Constants();
-    
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
@@ -23,23 +55,41 @@ class GetStarted extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
-
             children: [
-              Image.asset('assets/get-started.png'),
-              const SizedBox(height: 30,),
+              FadeTransition(
+                opacity: _fadeInAnimation!,
+                child: Image.asset('assets/get-started.png', height: 200, width: 200),
+              ),
+              const SizedBox(height: 30),
               GestureDetector(
-                onTap: (){
-                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> const Home()));
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Home()),
+                  );
                 },
-                child: Container(
-                  height: 50,
-                  width: size.width * 0.7,
-                  decoration: BoxDecoration(
-                    color: myConstants.primaryColor,
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
-                  ),
-                  child: const Center(
-                    child: Text('Get started', style: TextStyle(color: Colors.white, fontSize: 18),),
+                child: SlideTransition(
+                  position: _slideAnimation!,
+                  child: Container(
+                    height: 50,
+                    width: size.width * 0.7,
+                    decoration: BoxDecoration(
+                      color: myConstants.primaryColor,
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 10,
+                          offset: Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Get Started',
+                        style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
                   ),
                 ),
               ),
