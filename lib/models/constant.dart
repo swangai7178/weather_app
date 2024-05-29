46b745a8c032d1e88e8e39fad68ba05635cb0642
unknown <swangai7178@gmail.com>
2024-05-29 07:59:33 +0300

    finishing up on the homepage look

--- lib/models/constant.dart
+++ lib/models/constant.dart
@@ -2,6 +2,6 @@
 import 'package:flutter/material.dart';
 
 class Constants{
-  final Color primaryColor = const Color(0xff90B2F9);
+  final Color primaryColor = const Color.fromARGB(255, 95, 137, 227);
   final Color secondaryColor = const Color(0xff90B2F8);
 }
